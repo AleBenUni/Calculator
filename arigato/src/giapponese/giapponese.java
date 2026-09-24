2f99fba08515f7674325b90c4dcb732256105b80
Matteo <m.testa2004@studenti.unibg.it>
2026-09-24 16:56:11 +0200

    Update giapponese.java

--- arigato/src/giapponese/giapponese.java
+++ arigato/src/giapponese/giapponese.java
@@ -2,21 +2,8 @@ package giapponese;
 
 public class giapponese 
 {
-	ciao a tutty
-	7ciao mondo by diego zanga
-	appaiai esecuzione pubica di alire rota
-	
-	ma per caso ti chiami viola?... perche vuoi il mio zebby in gola
+
 			
-			
-			te ma fra il tipo in parte a te per noi è gay cioe mi ama gia
-<<<<<<< Updated upstream
-			prima mi ha chiesto se potesse unirsi con noi
-=======
-			
-			
-			
-			
-				ALICE HAI ROTTO LE PALLELERMLEKLRJWERWFENJOWREOQWRE
->>>>>>> Stashed changes
+			ESECUZIONE PUBICA DI ALICE ROTA IL 24/09 ALLE 17 SIETE TUTTI INVITATI
+
 }
