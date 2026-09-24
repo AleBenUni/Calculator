3bb8fbcd62f963ea62ab3358b7c8f4adc551d1d6
Matteo <m.testa2004@studenti.unibg.it>
2026-09-24 16:53:35 +0200

    Update giapponese.java

--- arigato/src/giapponese/giapponese.java
+++ arigato/src/giapponese/giapponese.java
@@ -7,4 +7,7 @@ public class giapponese
 	appaiai esecuzione pubica di alire rota
 	
 	ma per caso ti chiami viola?... perche vuoi il mio zebby in gola
+			
+			
+			te ma fra il tipo in parte a te per noi è gay cioe mi ama gia
 }
